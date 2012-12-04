49e62f1be36833a9cefa6ded562a6d84d0590ad8
Matt "Hayashi" Windsor <matt.windsor@ury.york.ac.uk>
2012-12-04 12:31:37 +0000

    location: de-URYise

--- schedule/models/location.py
+++ schedule/models/location.py
@@ -1,24 +1,26 @@
 """A location a show can be broadcast from."""
 
+from django.conf import settings
 from django.db import models
-from urysite import model_extensions as exts
 
 
 class Location(models.Model):
-    """A location in URY.
+    """
+    A location in URY.
 
     Locations are generally used to tag the origins of shows, for
     purposes such as webcam auto-configuration and location clash
     avoidance.
 
     """
-
-    class Meta:
-        db_table = 'location'  # in schema 'schedule'
-        app_label = 'schedule'
-
-    id = exts.primary_key_from_meta(Meta)
-
+    if hasattr(settings, 'LOCATION_DB_ID_COLUMN'):
+        db_table = settings.LOCATION_DB_ID_COLUMN
     name = models.TextField(
         db_column='location_name',
-        help_text='The human-readable name of this location.')
+        help_text='The human-readable name of this location.'
+    )
+
+    class Meta:
+        if hasattr(settings, 'LOCATION_DB_TABLE'):
+            db_table = settings.LOCATION_DB_TABLE
+        app_label = 'schedule'
