d42534826943ed382e904930b0e77208a73df548
Matt "Hayashi" Windsor <matt.windsor@ury.york.ac.uk>
2013-02-19 19:06:36 +0000

    incomplete poking of schedule tabulator

--- schedule/utils/week_table.py
+++ schedule/utils/week_table.py
@@ -8,18 +8,13 @@ functionality is a true hero.
 
 """
 
-import datetime
-
 from django.utils import timezone
 
 from .. import utils
 
 
-EMPTY_DELTA = datetime.timedelta(days=0)
-
-
 # This is a very common timedelta, so we use it as a constant.
-DAY = datetime.timedelta(days=1)
+DAY = timezone.timedelta(days=1)
 
 
 # The amount to add to the day number to get the schedule table column
@@ -92,16 +87,19 @@ def tabulate(schedule):
         the number of rows they span.  Duplicated entries (those that carry on
         from the previous row) are marked with None.
     """
-    nlstart = nld(schedule.start)
+    data = schedule.data
+    if isinstance(data, basestring):
+        # This is just an error signifier, pass it through.
+        table = data
+    else:
+        nlstart = nld(schedule.start)
+        data_lists, partitions = split_days(nlstart, schedule.data)
+        table = empty_table(nlstart, partitions, len(data_lists))
+        populate_table(table, data_lists)
+    return table
 
-    data_lists, partitions = split_days(nlstart, schedule.data)
 
-    table = empty_table(nlstart, partitions, len(data_lists))
-
-    return populate_table(table, data_lists)
-
-
-def populate_table(table, data_lists):
+def populate_table(nlstart, table, data_lists):
     """Populates empty schedule tables with data from the given lists.
 
     Args:
@@ -113,7 +111,11 @@ def populate_table(table, data_lists):
         the populated table, which may or may not be the same object as table
         depending on implementation.
     """
+    def row_date(row, day_offset):
+        return table[row][SCHEDULE_TIME_COL] + day_offset
+
     for i, day in enumerate(data_lists):
+        day_offset = timezone.timedelta(days=i)
         current_row = 0
         for slot in day:
             # How much local time does this slot take up?
@@ -122,7 +124,7 @@ def populate_table(table, data_lists):
             # Work out how many rows this slot fits into.
             add_rows = 0
             try:
-                while table[current_row + add_rows][SCHEDULE_TIME_COL] < nlend:
+                while row_date(current_row + add_rows, day_offset) < nlend:
                     add_rows += 1
             except IndexError:
                 # This usually means this show crosses over the day boundary;
@@ -131,7 +133,7 @@ def populate_table(table, data_lists):
 
             # If our partitioning is sound, then the slot must fit exactly into
             # one or more rows.
-            if table[current_row + add_rows][SCHEDULE_TIME_COL] >= nlend:
+            if row_date(current_row + add_rows, day_offset) >= nlend:
                 raise utils.exceptions.ScheduleInconsistencyError(
                     'Partitioning unsound - show exceeds partition bounds.'
                 )
@@ -164,44 +166,51 @@ def split_days(nlstart, data):
     """
     done_day_lists = []
     day_list = []
-    partitions = set()
+    partitions = set([timezone.timedelta(days=0)])
 
-    day_start = data[0].start_date
+    day_start = nlstart
+    day_end = day_start + DAY
 
     for slot in data:
-        # This is a while so it deals properly with the case of shows
-        # straddling multiple days; each day will be populated in turn.
-        while utils.range.dst_add(day_start, DAY) >= slot.start_date:
-            day, current_list = rotate_day(day_start, done_day_lists, day_list)
-            # Make sure that day boundaries are marked as partitions too
-            partitions.add(nld(day))
+        nlslot = nld(slot.start_time)
+        # If the next slot is outside the day we're looking at. rotate it.
+        # To deal with shows straddling multiple days, check for multiple
+        # rotations (hence the while loop).
+        while day_end <= nlslot:
+            day_start, day_end = day_end, day_start + DAY
+            day_list = rotate_day(day_start, day_list, done_day_lists)
 
-        current_list.append(slot)
-        partitions.add(nld(slot.start_time) - nlstart)
+        day_list.append(slot)
+        partitions.add(nlslot - day_start)
 
     # Finish off by pushing the last day onto the list, as nothing else will
     done_day_lists.append(day_list)
     return done_day_lists, partitions
 
 
-def rotate_day(day_start, day_list, done_day_lists):
+def rotate_day(day_end, day_list, done_day_lists):
     """Ends the current day and sets things up ready to process the next day.
 
     Args:
-        day_start: the (aware) datetime of the start of the day that is about
-            to finish
+        day_end: the (naive local) datetime of the end of the day that is about
+            to finish (and the start of the next day)
         day_list: the completed list of timeslots for the day being finished.
         done_day_lists: the list of completed day lists (in chronological
             order) to push the new day onto
     Returns:
-        a tuple containing the new day start and the new day list, which may or
-        may not be empty depending on show day crossover.
+        the initial list for the new day, which may or may not be empty
+        depending on show day crossover.
     """
     # We should NEVER be storing an empty list, as every day should at least
     # have one show (the filler show).
     if not day_list:
         raise utils.exceptions.ScheduleInconsistencyError(
-            'split_days encountered empty day list, is filler working?'
+            'split_days encountered empty day list, is filler working? '
+            '(day_end={} day_list={} done_day_lists={})'.format(
+                day_end,
+                day_list,
+                done_day_lists
+            )
         )
 
     done_day_lists.append(day_list)
@@ -209,10 +218,8 @@ def rotate_day(day_start, day_list, done_day_lists):
     # We need to see if the last show we processed straddles the boundary
     # between the two days and, if so, make sure it appears at the start of the
     # new list too.
-    last_show = done_day_lists[-1][-1]
-    new_list = [last_show] if last_show.end_time().date() > day_start else []
-
-    return utils.range.dst_add(day_start, DAY), new_list
+    last_show = day_list[-1]
+    return [last_show] if nld(last_show.end_time()) > day_end else []
 
 
 def empty_table(nlstart, partitions, n_cols):
@@ -228,7 +235,8 @@ def empty_table(nlstart, partitions, n_cols):
         an empty schedule table ready for population with show data,
         implemented as a list of row lists.
         The table will contain len(partitions) rows, each containing the naive
-        time of their occurrence, their offset from nlstart, and then num_cols
-        instances of None ready to be filled with schedule data.
+        time of their occurrence (on the first day of the schedule; add day
+        offsets for the other days), their offset from nlstart, and then
+        num_cols instances of None ready to be filled with schedule data.
     """
-    return [[(nlstart + i).time()] + ([None] * n_cols) for i in partitions]
+    return [[(nlstart + i)] + ([None] * n_cols) for i in partitions]
